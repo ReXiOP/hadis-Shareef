bdfe0da313e06a8f26dc68443834411efc321005
Sajid <108050827+ReXiOP@users.noreply.github.com>
2025-03-16 21:38:36 +0600

    u

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    allowedHosts: [
-      '3423-118-179-166-161.ngrok-free.app',  // Add your Ngrok URL here
-      'localhost',  // You can also add localhost for local development
-      '127.0.0.1'
-    ],
-  },
+  // server: {
+  //   allowedHosts: [
+  //     '3423-118-179-166-161.ngrok-free.app',  // Add your Ngrok URL here
+  //     'localhost',  // You can also add localhost for local development
+  //     '127.0.0.1'
+  //   ],
+  // },
 })
